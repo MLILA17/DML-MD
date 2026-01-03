ae22f8c2a354629ec83fd48dc726fe4f9048b144
DML <daudymussa1705@gmail.com>
2026-01-03 10:46:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// Secured by Fredi AI
-// Fredi AI Protection Layer
-require("./Dml/index.js"); // Fredi AI
-// Fredi AI Obfuscation Complete
-// Protected by Fredi AI - 2026, 12:29:58 PM
+// Secured by DML
+// DML Protection Layer
+require("./Dml/index.js"); // DML
+// DML Obfuscation Complete
+// Protected by DML - 2026, 12:29:58 PM
